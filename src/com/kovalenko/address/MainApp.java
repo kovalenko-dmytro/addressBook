ee09652ca66de3a1e7dcac1435422ea1b16e82c5
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2019-08-14 08:02:00 +0300

    create views

---
+++ src/com/kovalenko/address/MainApp.java
@@ -0,0 +1,61 @@
+package com.kovalenko.address;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        primaryStage.setTitle("Address Book");
+
+        initRootLayout();
+        setStage();
+        initPersonOverView();
+    }
+
+    private void initRootLayout() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+        try {
+            rootLayout = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initPersonOverView() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+        try {
+            AnchorPane anchorPane = loader.load();
+            rootLayout.setCenter(anchorPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setStage() {
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+}
