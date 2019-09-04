c438af86c67e969e6219d25fbb2e5aefacd00e99
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2019-09-04 08:40:15 +0300

    refactor

--- src/com/kovalenko/address/MainApp.java
+++ src/com/kovalenko/address/MainApp.java
@@ -17,14 +17,15 @@ public class MainApp extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         primaryStage = stage;
         primaryStage.setTitle("Address Book");
 
         initRootLayout();
-        setStage();
         initPersonOverView();
+        setStage();
     }
 
     private void initRootLayout() {
